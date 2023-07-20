edffbc080a989fc583f3ac4b7e6d7b6196a71cca
williamjparra <w3webservices.net@gmail.com>
2023-07-20 15:05:07 -0400

    gatsby removed dev ssr

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,9 +8,6 @@ const config: GatsbyConfig = {
 		lang: `es`,
 		description: `ecograsas procesadores de aceite de palma para reposteria y elaboracion de jabon`,
 	},
-	flags: {
-		DEV_SSR: true,
-	},
 	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
 	// If you use VSCode you can also use the GraphQL plugin
 	// Learn more at: https://gatsby.dev/graphql-typegen
