ef8098db71efded19dc6b94af347ab54956c51a5
williamjparra <w3webservices.net@gmail.com>
2023-09-04 08:44:38 -0400

    solved typos

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,7 +6,7 @@ export default function Register() {
 	return (
 		<>
 			<Helmet>
-				<title>{"ecograsas - Registrase"}</title>
+				<title>{"ecograsas - Registrarse"}</title>
 				<meta
 					content={
 						"registrase para comprar en Procesadora Ecograsas es una empresa venezolana especializada en el procesamiento y comercialización de productos de aceite y grasa. Comprometidos con la sostenibilidad y la responsabilidad ambiental, buscamos ofrecer productos de alta calidad mientras minimizamos nuestro impacto ecológico. Nuestra misión es ser líderes en la provisión de productos de aceite y grasa, priorizando la sostenibilidad ambiental. Visualizamos un futuro en el que las prácticas sostenibles y los productos de alta calidad vayan de la mano, contribuyendo a un futuro más verde y sostenible."
