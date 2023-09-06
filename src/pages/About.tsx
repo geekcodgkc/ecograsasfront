7f8a33c1813979c037c999338e9bcc622a44350e
williamjparra <w3webservices.net@gmail.com>
2023-09-06 10:20:43 -0400

    solved typos

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AboutContainer from "../containers/AboutContainer";
 
-// container de about Falta procesos
+// container de about Falta procesos y mas alucion
 
 export default function About() {
 	return <AboutContainer />;
